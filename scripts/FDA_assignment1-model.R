498049f82ff9ab7ac10d3b25faf55bfc38723533
HDking <h.c.dinh1@gmail.com>
2015-05-09 13:49:49 +0200

    de coeffecienten van de lijnen er uit gehaald

--- scripts/FDA_assignment1-model.R
+++ scripts/FDA_assignment1-model.R
@@ -22,7 +22,11 @@ abline(real.function, col="black", lwd=5)
 xAll = vector()
 yAll = vector()
 
+
 oneSample <- function(x,n){
+  mat = matrix(, nrow=n+1, ncol=2)
+  mat2 = matrix(, nrow=n+1, ncol=3)
+  mat3 = matrix(, nrow=n+1, ncol=4)
   for (i in 1:n){
     xSample <- sample(x, 20, replace=TRUE, prob = NULL)    
     
@@ -33,42 +37,76 @@ oneSample <- function(x,n){
     #points(xSample,ySample) is a test if the points are actually on the line
     points(xSample,ySample)
     linear.model = lm(ySample~xSample)
-    #abline(linear.model, col="red", lwd=1)
+    abline(linear.model, col="red", lwd=1)
+    mat[i,]=coef(linear.model)
     
     fit2 <- lm(ySample~xSample + I(xSample^2))
     #show the individual lines of the second order regression lines
     #points(xSample, predict(fit2), type="l", col="#FF0A85", lwd=1)
+    mat2[i,]=coef(fit2)
     
     fit3 <- lm(ySample~xSample + I(xSample^2) + I(xSample^3))
     #show the individual lines of the third order regression lines
     #points(xSample,predict(fit3), type="l", col="#66FF33", lwd=1)
+    mat3[i,]=coef(fit3)
     
     #Defines all the x-values from the samples and sorts them for smooth lines
     xAll = c(xAll, xSample)
     xAll = sort(xAll)
     yAll = real(xAll)
+    
   }
   #Linear regression lines
   avgReg = lm(yAll~xAll)
-  if (n==10) {abline(avgReg, col="darkgreen", lwd=5)}
-  if (n ==100) {abline(avgReg, col="darkgreen", lwd=5)}
-  if (n == 1000) {abline(avgReg, col="darkgreen", lwd=5)}
+  if (n==10) {
+    abline(lin.reg <- avgReg, col="darkgreen", lwd=5)
+    mat[11,] = coef(lin.reg)
+  }
+  if (n ==25) {
+    abline(lin.reg <- avgReg, col="darkgreen", lwd=5)
+    mat[26,] = coef(lin.reg)
+  }
+  if (n == 250) {
+    abline(lin.reg <- avgReg, col="darkgreen", lwd=5)
+    mat[251,] = coef(lin.reg)
+  }
   #quadratic regression lines
   xAll2 = xAll^2
   avgReg2 = lm(yAll~xAll + I(xAll2))
-  if (n==10) {lines(xAll, predict(avgReg2), col="#0A47FF", lwd=5)}
-  if (n ==100) {lines(xAll, predict(avgReg2), col="#0A47FF", lwd=5)}
-  if (n == 1000) {lines(xAll, predict(avgReg2), col="#0A47FF", lwd=5)}
+  if (n==10) {
+    lines(xAll, predict(avgReg2), col="#0A47FF", lwd=5)
+    mat2[11,] = coef(avgReg2)
+  }
+  if (n ==25) {
+    lines(xAll, predict(avgReg2), col="#0A47FF", lwd=5)
+    mat2[26,] = coef(avgReg2)
+  }
+  if (n == 250) {
+    lines(xAll, predict(avgReg2), col="#0A47FF", lwd=5)
+    mat2[250,] = coef(avgReg2)
+  }
   #cubic regression lines
   xAll2 = xAll^2
   xAll3 = xAll^3
   avgReg3 = lm(yAll~xAll + I(xAll2) + I(xAll3))
-  if (n==10) {lines(xAll, predict(avgReg3), col="darkorange", lwd=5)}
-  if (n ==100) {lines(xAll, predict(avgReg3), col="darkorange", lwd=5)}
-  if (n == 1000) {lines(xAll, predict(avgReg3), col="darkorange", lwd=5)}
+  if (n==10) {
+    lines(xAll, predict(avgReg3), col="darkorange", lwd=5)
+    mat3[11,] =coef(avgReg3)
+  }
+  if (n ==25) {
+    lines(xAll, predict(avgReg3), col="darkorange", lwd=5)
+    mat3[26,] = coef(avgReg3)
+  }
+  if (n == 250) {
+    lines(xAll, predict(avgReg3), col="darkorange", lwd=5)
+    mat3[251,] =coef(avgReg3)
+  }
+  return(mat)
+  #return(mat2)
+  #return(mat3)
 }
 
 #run the function
 tenSamples = oneSample(x,10)
-#hundredSamples = oneSample(x,100)
-#thousandSamples = oneSample(x,1000)
+#hundredSamples = oneSample(x,25)
+#thousandSamples = oneSample(x,250)
