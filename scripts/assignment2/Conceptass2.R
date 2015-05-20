a5df19229f8c117c180ff769774d8c7554f7d0f8
Vishan07 <vishanbaldew@icloud.com>
2015-05-20 15:07:32 +0200

    Add concept
    Add concept

---
+++ scripts/assignment2/Conceptass2.R
@@ -0,0 +1,18 @@
+normVec <- function (x) sqrt(sum(x^2))
+
+Rfunct <- function(x) {
+  exp(-a * normVec(x)
+}
+
+y <- runif(1000,-1,1) 
+
+#functie Rfunct <- function(x) {
+                  #exp(-a * normVec(x)
+
+#1000 runif points, -1,1 for each i in p[1:32]. 
+#Search for nearest x from 0. 
+#take corresponding values of the dimension p; follows from the function
+#calculate Euclidean distance 
+#plot value
+
+#This is for point 1 and 2, from 3 on we are still figuring it out
